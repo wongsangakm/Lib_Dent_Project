0671ee8d6c6a6f6da71a8414242408490a0b9c63
Kamonlak-Wongsanga <kamonlak.wo@kkumail.com>
2025-04-30 08:54:26 +0700

    add footer add logo change folder

---
+++ frontend/src/component/Logo.vue
@@ -0,0 +1,7 @@
+<template>
+  <img :src="logoSrc" alt="Faculty Logo" class="w-24 h-24 mx-auto" />
+</template>
+
+<script setup>
+const logoSrc = new URL('@/assets/faculty.svg', import.meta.url).href;
+</script>
