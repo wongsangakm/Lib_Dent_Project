8d203f2ea945a7e0cda1c9c676e4cdb56ad0e7e3
Kamonlak-Wongsanga <kamonlak.wo@kkumail.com>
2025-05-13 12:44:28 +0700

    fetch all books, add search bar

--- frontend/src/pages/AdminAllbooks.vue
+++ frontend/src/pages/AdminAllbooks.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="container mx-auto px-4 py-12">
     <h1 class="text-2xl font-bold text-center mb-8">All Books</h1>
-
     <div class="flex justify-between items-center mb-6">
       <h1 class="text-2xl font-bold">All Books</h1>
       <router-link
@@ -12,9 +11,28 @@
       </router-link>
     </div>
 
+
+
+    <div class="flex justify-between items-center mb-4">
+      <input
+        v-model="searchText"
+        type="text"
+        placeholder="Search by title or publisher"
+        class="border px-4 py-2 rounded w-full max-w-md"
+      />
+    </div>
+    <p class="text-sm text-gray-600 mb-4">
+      Showing {{ filteredBooks.length }} book<span
+        v-if="filteredBooks.length !== 1"
+        >s</span
+      >
+    </p>
+
+    <!--Search Input -->
+
     <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6">
       <div
-        v-for="book in allBooks"
+        v-for="book in filteredBooks"
         :key="book.id"
         class="bg-white rounded-xl shadow-md p-4 flex flex-col items-center"
       >
@@ -24,10 +42,12 @@
           class="w-[120px] h-[180px] object-cover rounded"
         />
         <p class="mt-3 text-center font-semibold text-gray-700 truncate w-full">
-          {{ book.title }}
+          {{ book.bookTitle }}
         </p>
         <p class="text-xs text-gray-500">{{ book.publisher }}</p>
-        <p class="text-sm font-bold text-purple-700 mt-1">{{ book.price }}</p>
+        <p class="text-sm font-bold text-purple-700 mt-1">
+          {{ book.price.toLocaleString() }} THB
+        </p>
 
         <router-link
           :to="`/admin/adminbookdetail/${book.id}`"
@@ -42,8 +62,24 @@
 
 <script setup>
 import { useFavouritesStore } from "@/stores/favourites";
-import { computed } from "vue";
+import { computed, onMounted, ref } from "vue";
 
 const favouritesStore = useFavouritesStore();
 const allBooks = computed(() => favouritesStore.allBooks);
+const searchText = ref("");
+
+// ✅ กรองตาม search
+const filteredBooks = computed(() => {
+  const keyword = searchText.value.toLowerCase();
+  return allBooks.value.filter(
+    (book) =>
+      book.bookTitle?.toLowerCase().includes(keyword) ||
+      book.publisher?.toLowerCase().includes(keyword)
+  );
+});
+
+// ✅ Fetch books on component mount
+onMounted(() => {
+  favouritesStore.fetchAllBooks();
+});
 </script>
