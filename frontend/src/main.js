99872099a7a82c016047dd246c622e859c31b06d
Kamonlak-Wongsanga <kamonlak.wo@kkumail.com>
2025-05-16 13:55:26 +0700

    fix admin permission

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,7 +5,6 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
 import App from './App.vue'
 import router from './router'
 
@@ -18,5 +17,5 @@ app.use(createPinia())
 pinia.use(piniaPluginPersistedstate);
 app.use(router)
 app.use(ElementPlus)
-
+app.use(pinia)
 app.mount('#app')
