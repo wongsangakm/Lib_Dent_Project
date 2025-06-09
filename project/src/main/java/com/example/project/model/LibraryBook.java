c47855dba9cb7e13070b5a1aa2df7a2a2634b3b3
Kamonlak-Wongsanga <kamonlak.wo@kkumail.com>
2025-06-09 10:56:36 +0700

    fix seq to iden (lib book)

--- project/src/main/java/com/example/project/model/LibraryBook.java
+++ project/src/main/java/com/example/project/model/LibraryBook.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 public class LibraryBook {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(columnDefinition = "TEXT")
